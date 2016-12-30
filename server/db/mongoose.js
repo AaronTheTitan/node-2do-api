1c33d0631db4b86d5d175db0dfa318d24eafe41c
AaronTheTitan <abeats20@gmail.com>
2016-12-30 00:05:06 -0600

    add mongolabl url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
+const dbUrl = "mongodb://abeezy:abeezy123ez@ds053764.mlab.com:53764/node_todo_api";
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(dbUrl || 'mongodb://localhost:27017/TodoApp');
 
 module.exports.mongoose = { mongoose };
